deaf59754c2f8e546e4142f95b2e184c617550e1
Eric Cuevas <ericcuevas@Erics-Mac-mini.local>
2023-07-27 16:30:20 -0700

    created a login screen with a general layout

--- lib/src/auth_feature/login_view.dart
+++ lib/src/auth_feature/login_view.dart
@@ -1,19 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+class AuthButton extends StatefulWidget {
+  final String? imageAssetPath;
+  final String text;
+  final VoidCallback onPressed;
+
+  const AuthButton({
+    this.imageAssetPath,
+    required this.text,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AuthButton> createState() => _AuthButtonState();
+}
+
+class _AuthButtonState extends State<AuthButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    );
+  }
+
+  void _onButtonPressed() {
+    _controller.forward();
+    Future.delayed(const Duration(milliseconds: 200), () {
+      _controller.reverse();
+    });
+    widget.onPressed();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: Tween<double>(
+        begin: 1.0,
+        end: .98,
+      ).animate(_controller),
+      child: ElevatedButton(
+        onPressed: _onButtonPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white.withOpacity(.2),
+          elevation: 1,
+          side: const BorderSide(width: 2, color: Colors.white60),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (widget.imageAssetPath != null)
+                Image.asset(
+                  widget.imageAssetPath!,
+                  width: 24,
+                  height: 24,
+                ),
+              if (widget.imageAssetPath != null) const SizedBox(width: 10),
+              Text(
+                widget.text,
+                style: GoogleFonts.nunito(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        centerTitle: true, // Center the title horizontally.
+        centerTitle: true,
         title: Text(
-          'Login with email',
+          'Sign in',
           style: GoogleFonts.nunito(
             fontSize: 20,
             fontWeight: FontWeight.bold,
@@ -32,12 +114,70 @@ class LoginView extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          // Set the background image using a Container with a DecorationImage
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/welcome-background.png'),
                 fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {
+                      // Add your "Forgot Password" logic here
+                    },
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  AuthButton(
+                    text: 'Login',
+                    onPressed: () {
+                      // Add your login logic here
+                    },
+                  ),
+                  const SizedBox(height: 20), // Add more spacing between buttons
+                  TextButton(
+                    onPressed: () {
+                      // Add your "Create Account" logic here
+                    },
+                    child: const Text(
+                      'New User? Create Account',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
